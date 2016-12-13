0ae8ea0541c90c437e0de4416adc27cf94630ad1
anjaxs <942403648@qq.com>
2016-12-13 12:59:01 +0800

    完成实验－

--- part2/experiment1/main2.c
+++ part2/experiment1/main2.c
@@ -1,8 +1,65 @@
+/*将两个有序的线性表进行合并，要求同样的数据只出现一次*/
 #include <stdio.h>
+#include <stdlib.h>
 #include "SqList.h"
+
+
+
+/**
+ * 合并线性表
+ * @param  L1 
+ * @param  L2 
+ * @return    新的线性表
+ */
+List MergeList(List * L1, List * L2);
 
 int main(void)
 {
-	printf("%d\n", max(15,20));
+	int list1[15] = {2, 5, 7, 8, 10, 14, 19, 22, 25, 30};
+	int list2[15] = {3, 5, 8, 9, 11, 18, 22, 28, 30, 32, 35};
+	List L1 = {list1, 10, 15};
+	List L2 = {list2, 11, 15};
+	List L3 = MergeList(&L1, &L2);
+	OutputList(&L3);
 	return 0;
+}
+
+List MergeList(List * L1, List * L2)
+{
+	List L3;
+	int i = 0, j = 0;
+	int L1Size = L1->size, L2Size = L2->size;
+	
+	InitList(&L3, L1Size + L2Size);
+	while (L1Size && L2Size) {
+		if (L1->list[i] <= L2->list[j]) {
+			InsertList(&L3, L1->list[i], L3.size);							
+			if (L1->list[i] == L2->list[j]) {      
+				j++;
+				L2Size--;	//L2->list 移到下一个元素
+			}
+			i++;
+			L1Size--;		//L1->list 移到下一个元素
+		} else {
+			InsertList(&L3, L2->list[j], L3.size);
+			j++;
+			L2Size--;	//L2->list 移到下一个元素
+		}
+	}
+
+	//剩余的Ｌ１
+	while (L1Size) {
+		InsertList(&L3, L1->list[i], L3.size);
+		i++;
+		L1Size--;		//L1->list 移到下一个元素
+	}
+
+	//剩余的Ｌ2
+	while (L2Size) {
+		InsertList(&L3, L2->list[j], L3.size);
+		j++;
+		L2Size--;		//L2->list 移到下一个元素
+	}
+
+	return L3;
 }
--- part2/experiment1/main3.c
+++ part2/experiment1/main3.c
@@ -1,4 +1,6 @@
+/*要求以较高的效率删除线性表中元素值在ｘ到ｙ（ｘ和ｙ自定）之间的所有元素*/
 #include <stdio.h>
+#include <stdlib.h>
 #include "SqList.h"
 
 int main(void)
